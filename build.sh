e0663b873fc7b9214539c81f5d79938531784293
Maxim Shkalin <dt5@bk.ru>
2013-11-05 17:31:16 +0000

    Update build script.

--- build.sh
+++ build.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# This file is build module distributive
-# from OpenCart installed module.
-
 INSTALL_PATH="../.."
 BUILD_PATH="./upload"
 
 # -------------------------------------------
 
 FILE_LIST=(
-	"system/third_party/exchange1c"
-	"vqmod/xml/exchange1c.xml"
-	"exchange1c/"
 	"admin/language/russian/module/exchange1c.php"
 	"admin/view/template/module/exchange1c.tpl"
 	"admin/controller/module/exchange1c.php"
 	"admin/model/module/exchange1c.php"
+	"system/third_party/exchange1c"
+	"vqmod/xml/exchange1c.xml"
+	"exchange1c/"
 )
 
 # -------------------------------------------
@@ -25,11 +22,12 @@ for FILE in "${FILE_LIST[@]}"; do
 	TO="$BUILD_PATH/$FILE"
 
 	if [ -f $FROM ] || [ -d $FROM ]; then
-		mkdir -p $(dirname $TO)
-		cp -r $FROM $TO
-		echo "[BUILD] $TO"
+		if [ ! -d $(dirname $TO) ]; then
+			mkdir -p $(dirname $TO)
+		fi
+		cp -arT $FROM $TO
 	else
-		echo "[ERROR] File not exists: $FROM"
+		"No such file or directory: $FROM"
 	fi
 done
 
