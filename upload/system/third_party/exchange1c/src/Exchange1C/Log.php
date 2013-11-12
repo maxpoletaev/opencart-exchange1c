15b0fd69316511715d0a43f1db436a20801a114a
Maxim Shkalin <dt5@bk.ru>
2013-11-12 21:53:15 +0000

    Update log system.

--- upload/system/third_party/exchange1c/src/Exchange1C/Log.php
+++ upload/system/third_party/exchange1c/src/Exchange1C/Log.php
@@ -12,9 +12,51 @@ class Log {
 	public static function write($message, $type = 'notify')
 	{
 		$logType = strtoupper($type);
-		echo "[$logType] $message \n";
+		
+		$date = date('Y-m-d');
+		$time = date('H:i:s');
 
-		// @TODO: write log to file
+		$string = "{{$time}} [$logType]: {$message} \n";
+
+		$handle = fopen(E1C_DIR . "/logs/{$date}.log", 'a');
+		fwrite($handle, $string);
+		fclose($handle);
+	}
+
+
+	/**
+	 * Write debug message.
+	 *
+	 * @var string $message
+	 * @return void
+	 */
+	public static function debug($message)
+	{
+		$trace = debug_backtrace();
+		
+		$class = $trace[1]['class'];
+		$method = $trace[1]['function'];
+		
+		$debugMessage = "{$message} ({$class}::{$method})";
+		static::write($debugMessage, 'debug');
+	}
+
+
+	/**
+	 * Write error message.
+	 *
+	 * @var string $message
+	 * @return void
+	 */
+	public static function error($message)
+	{
+		$trace = debug_backtrace();
+		
+		$file = $trace[1]['file'];
+		$line = $trace[1]['line'];
+
+		$errorMessage = "{$message} ({$file}:{$line})";
+		static::write($errorMessage, 'error');
 	}
 
 }
