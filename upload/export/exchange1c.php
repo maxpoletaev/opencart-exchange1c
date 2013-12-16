2d6e064774c9f86e1595fc1b685c275c0e497a3f
Ilia Maslakov <il.smind@gmail.com>
2013-12-16 17:38:31 +0400

    cleanup export/exchange1c.php
    Signed-off-by: Ilia Maslakov <il.smind@gmail.com>

--- upload/export/exchange1c.php
+++ upload/export/exchange1c.php
@@ -7,21 +7,21 @@ define('VERSION', '1.5.5.1.1');
 require_once('../admin/config.php');
 
 if (file_exists('../vqmod/vqmod.php')) {
-	require_once('../vqmod/vqmod.php');
-	VQMod::bootup();
+    require_once('../vqmod/vqmod.php');
+    VQMod::bootup();
 
-	require_once(VQMod::modCheck(DIR_SYSTEM . 'startup.php'));
-	require_once(VQMod::modCheck(DIR_SYSTEM . 'library/currency.php'));
-	require_once(VQMod::modCheck(DIR_SYSTEM . 'library/user.php'));
-	require_once(VQMod::modCheck(DIR_SYSTEM . 'library/weight.php'));
-	require_once(VQMod::modCheck(DIR_SYSTEM . 'library/length.php'));
+    require_once(VQMod::modCheck(DIR_SYSTEM . 'startup.php'));
+    require_once(VQMod::modCheck(DIR_SYSTEM . 'library/currency.php'));
+    require_once(VQMod::modCheck(DIR_SYSTEM . 'library/user.php'));
+    require_once(VQMod::modCheck(DIR_SYSTEM . 'library/weight.php'));
+    require_once(VQMod::modCheck(DIR_SYSTEM . 'library/length.php'));
 }
 else {
-	require_once(DIR_SYSTEM . 'startup.php');
-	require_once(DIR_SYSTEM . 'library/currency.php');
-	require_once(DIR_SYSTEM . 'library/user.php');
-	require_once(DIR_SYSTEM . 'library/weight.php');
-	require_once(DIR_SYSTEM . 'library/length.php');
+    require_once(DIR_SYSTEM . 'startup.php');
+    require_once(DIR_SYSTEM . 'library/currency.php');
+    require_once(DIR_SYSTEM . 'library/user.php');
+    require_once(DIR_SYSTEM . 'library/weight.php');
+    require_once(DIR_SYSTEM . 'library/length.php');
 }
 
 // Registry
@@ -41,51 +41,51 @@ $registry->set('db', $db);
 
 // Settings
 $query = $db->query("SELECT * FROM " . DB_PREFIX . "setting");
- 
+
 foreach ($query->rows as $setting) {
-	if (!$setting['serialized']) {
-		$config->set($setting['key'], $setting['value']);
-	} else {
-		$config->set($setting['key'], unserialize($setting['value']));
-	}
+    if (!$setting['serialized']) {
+        $config->set($setting['key'], $setting['value']);
+    } else {
+        $config->set($setting['key'], unserialize($setting['value']));
+    }
 }
 
-// Log 
+// Log
 $log = new Log($config->get('config_error_filename'));
 $registry->set('log', $log);
 
 // Error Handler
 function error_handler($errno, $errstr, $errfile, $errline) {
-	global $config, $log;
+    global $config, $log;
 
-	if (0 === error_reporting()) return TRUE;
-	switch ($errno) {
-		case E_NOTICE:
-		case E_USER_NOTICE:
-			$error = 'Notice';
-			break;
-		case E_WARNING:
-		case E_USER_WARNING:
-			$error = 'Warning';
-			break;
-		case E_ERROR:
-		case E_USER_ERROR:
-			$error = 'Fatal Error';
-			break;
-		default:
-			$error = 'Unknown';
-			break;
-	}
+    if (0 === error_reporting()) return TRUE;
+    switch ($errno) {
+        case E_NOTICE:
+        case E_USER_NOTICE:
+            $error = 'Notice';
+            break;
+        case E_WARNING:
+        case E_USER_WARNING:
+            $error = 'Warning';
+            break;
+        case E_ERROR:
+        case E_USER_ERROR:
+            $error = 'Fatal Error';
+            break;
+        default:
+            $error = 'Unknown';
+            break;
+    }
 
-	if ($config->get('config_error_display')) {
-		echo '<b>' . $error . '</b>: ' . $errstr . ' in <b>' . $errfile . '</b> on line <b>' . $errline . '</b>';
-	}
-	
-	if ($config->get('config_error_log')) {
-		$log->write('PHP ' . $error . ':  ' . $errstr . ' in ' . $errfile . ' on line ' . $errline);
-	}
+    if ($config->get('config_error_display')) {
+        echo '<b>' . $error . '</b>: ' . $errstr . ' in <b>' . $errfile . '</b> on line <b>' . $errline . '</b>';
+    }
 
-	return TRUE;
+    if ($config->get('config_error_log')) {
+        $log->write('PHP ' . $error . ':  ' . $errstr . ' in ' . $errfile . ' on line ' . $errline);
+    }
+
+    return TRUE;
 }
 
 // Error Handler
@@ -98,7 +98,7 @@ $registry->set('request', $request);
 // Response
 $response = new Response();
 $response->addHeader('Content-Type: text/html; charset=utf-8');
-$registry->set('response', $response); 
+$registry->set('response', $response);
 
 // Session
 $registry->set('session', new Session());
@@ -112,23 +112,23 @@ $registry->set('document', new Document());
 // Language
 $languages = array();
 
-$query = $db->query("SELECT * FROM " . DB_PREFIX . "language"); 
+$query = $db->query("SELECT * FROM " . DB_PREFIX . "language");
 
 foreach ($query->rows as $result) {
-	$languages[$result['code']] = array(
-		'language_id'	=> $result['language_id'],
-		'name'		=> $result['name'],
-		'code'		=> $result['code'],
-		'locale'	=> $result['locale'],
-		'directory'	=> $result['directory'],
-		'filename'	=> $result['filename']
-	);
+    $languages[$result['code']] = array(
+        'language_id'    => $result['language_id'],
+        'name'        => $result['name'],
+        'code'        => $result['code'],
+        'locale'    => $result['locale'],
+        'directory'    => $result['directory'],
+        'filename'    => $result['filename']
+    );
 }
 
 $config->set('config_language_id', $languages[$config->get('config_admin_language')]['language_id']);
 
 $language = new Language($languages[$config->get('config_admin_language')]['directory']);
-$language->load($languages[$config->get('config_admin_language')]['filename']);	
+$language->load($languages[$config->get('config_admin_language')]['filename']);
 $registry->set('language', $language);
 
 // Currency
@@ -150,54 +150,54 @@ $controller = new Front($registry);
 // Router
 if (isset($request->get['mode']) && $request->get['type'] == 'catalog') {
 
-	switch ($request->get['mode']) {
-		case 'checkauth':
-			$action = new Action('module/exchange1c/modeCheckauth');
-		break;
-		
-		case 'init':
-			$action = new Action('module/exchange1c/modeCatalogInit');
-		break;
+    switch ($request->get['mode']) {
+        case 'checkauth':
+            $action = new Action('module/exchange1c/modeCheckauth');
+        break;
 
-		case 'file':
-			$action = new Action('module/exchange1c/modeFile');
-		break;
+        case 'init':
+            $action = new Action('module/exchange1c/modeCatalogInit');
+        break;
 
-		case 'import':
-			$action = new Action('module/exchange1c/modeImport');
-		break;
+        case 'file':
+            $action = new Action('module/exchange1c/modeFile');
+        break;
 
-		default:
-			echo "success\n";
-	}
-	
+        case 'import':
+            $action = new Action('module/exchange1c/modeImport');
+        break;
+
+        default:
+            echo "success\n";
+    }
+
 } else if (isset($request->get['mode']) && $request->get['type'] == 'sale') {
-	
-	switch ($request->get['mode']) {
-		case 'checkauth':
-			$action = new Action('module/exchange1c/modeCheckauth');
-		break;
-		
-		case 'init':
-			$action = new Action('module/exchange1c/modeSaleInit');
-		break;
 
-		case 'query':
-			$action = new Action('module/exchange1c/modeQueryOrders');
-		break;
+    switch ($request->get['mode']) {
+        case 'checkauth':
+            $action = new Action('module/exchange1c/modeCheckauth');
+        break;
 
-		default:
-			echo "success\n";
-	}
+        case 'init':
+            $action = new Action('module/exchange1c/modeSaleInit');
+        break;
+
+        case 'query':
+            $action = new Action('module/exchange1c/modeQueryOrders');
+        break;
+
+        default:
+            echo "success\n";
+    }
 
 } else {
-	echo "success\n";
-	exit;
+    echo "success\n";
+    exit;
 }
 
 // Dispatch
 if (isset($action)) {
-	$controller->dispatch($action, new Action('error/not_found'));
+    $controller->dispatch($action, new Action('error/not_found'));
 }
 
 // Output
