3c5cdc2f2dda2ac91b5f1a5bc69b35424606bdd3
Ilia Maslakov <il.smind@gmail.com>
2013-12-16 17:27:38 +0400

    cleanup english/module/exchange1c.php russian/module/exchange1c.php
    Signed-off-by: Ilia Maslakov <il.smind@gmail.com>

--- upload/admin/language/english/module/exchange1c.php
+++ upload/admin/language/english/module/exchange1c.php
@@ -1,54 +1,52 @@
 <?php
 // Heading
-$_['heading_title']	= 'Exchange 1C v8.x';
+$_['heading_title'] = 'Exchange 1C v8.x';
 
 // Text
-$_['text_module']		= 'Modules';
-$_['text_success']		= 'Settings saved!';
+$_['text_module'] = 'Modules';
+$_['text_success'] = 'Settings saved!';
 
-$_['text_tab_general']		= 'General';
-$_['text_tab_product']		= 'Products';
-$_['text_tab_order']		= 'Orders';
-$_['text_tab_manual']		= 'Manual import';
+$_['text_tab_general'] = 'General';
+$_['text_tab_product'] = 'Products';
+$_['text_tab_order'] = 'Orders';
+$_['text_tab_manual'] = 'Manual import';
 
-$_['text_upload_success']	= 'Import completed';
-$_['text_upload_error']		= 'Unknown error';
-$_['text_max_filesize']		= 'Max upload size %s MB';
+$_['text_upload_success'] = 'Import completed';
+$_['text_upload_error'] = 'Unknown error';
+$_['text_max_filesize'] = 'Max upload size %s MB';
 
-$_['text_empty']		= 'Settings are missing';
-$_['text_homepage']		= 'Module homepage';
-
+$_['text_empty'] = 'Settings are missing';
+$_['text_homepage'] = 'Module homepage';
 
 // Entry
-$_['entry_username']		= 'Login:';
-$_['entry_password']		= 'Password:';
-$_['entry_status']		= 'Status:';
-$_['entry_allow_ip']		= 'Allow IPs (Share a line break. If empty, all addresses are allowed.):';
+$_['entry_username'] = 'Login:';
+$_['entry_password'] = 'Password:';
+$_['entry_status'] = 'Status:';
+$_['entry_allow_ip'] = 'Allow IPs (Share a line break. If empty, all addresses are allowed.):';
 
-
-$_['entry_price_type']		= 'Price type:';
-$_['entry_customer_group']	= 'Customer group:';
-$_['entry_quantity']		= 'Quantity:';
-$_['entry_priority']		= 'Priority:';
-$_['entry_flush_product']	= 'Flush products:';
-$_['entry_flush_category']	= 'Flush categories:';
-$_['entry_flush_manufacturer']	= 'Flush manufacturers:';
-$_['entry_flush_quantity']	= 'Flush quantity:';
-$_['entry_flush_attribute']	= 'Flush attributes:';
-$_['entry_fill_parent_cats']	= 'Fill parent categories:';
-$_['entry_seo_url']		= 'Generate SEO URL (depends on <a href="http://opencartforum.ru/files/file/332-deadcow-seo-v21-automod/" target="_blank">Deadcow SEO</a>):';
-$_['entry_full_log']		= 'Enable log';
-$_['entry_apply_watermark']	= 'Apply watermark';
+$_['entry_price_type'] = 'Price type:';
+$_['entry_customer_group'] = 'Customer group:';
+$_['entry_quantity'] = 'Quantity:';
+$_['entry_priority'] = 'Priority:';
+$_['entry_flush_product'] = 'Flush products:';
+$_['entry_flush_category'] = 'Flush categories:';
+$_['entry_flush_manufacturer'] = 'Flush manufacturers:';
+$_['entry_flush_quantity'] = 'Flush quantity:';
+$_['entry_flush_attribute'] = 'Flush attributes:';
+$_['entry_fill_parent_cats'] = 'Fill parent categories:';
+$_['entry_seo_url'] = 'Generate SEO URL (depends on <a href="http://opencartforum.ru/files/file/332-deadcow-seo-v21-automod/" target="_blank">Deadcow SEO</a>):';
+$_['entry_full_log'] = 'Enable log';
+$_['entry_apply_watermark'] = 'Apply watermark';
 $_['text_image_manager'] = 'Image Manager';
-$_['text_browse']        = 'Browse Files';
-$_['text_clear']         = 'Clear Image';
+$_['text_browse'] = 'Browse Files';
+$_['text_clear'] = 'Clear Image';
 
-$_['entry_order_status']	= 'Uploaded orders status:';
-$_['entry_order_notify']	= 'Notify users of status change:';
-$_['entry_order_currency']	= 'Orders currency:';
+$_['entry_order_status'] = 'Uploaded orders status:';
+$_['entry_order_notify'] = 'Notify users of status change:';
+$_['entry_order_currency'] = 'Orders currency:';
 
-$_['entry_upload']		= 'Select file:';
-$_['button_upload']		= 'Upload';
+$_['entry_upload'] = 'Select file:';
+$_['button_upload'] = 'Upload';
 
 // Error
-$_['error_permission']		= 'Premission denied!';
+$_['error_permission'] = 'Premission denied!';
